e77beb3ff4b4b0a2192db9a2c6d304fe4cc4a7c0
UCAS-John <john.wangwang@ucas-edu.net>
2025-01-24 13:34:00 -0700

    random password generator

--- Random Password Generator/main.py
+++ Random Password Generator/main.py
@@ -10,58 +10,156 @@
     -numbers
     -special characters
 """
-def generate_password(length: int, upper: bool, lower: bool, number: bool, special: bool):
-    pass
 
-def main():
+import string
+import random
 
-    print("\nRandom password generator\n")
+def lower_case(random_char):
 
     while True:
-        try:
-            length = int(input("Specify the length of your passowrd\n>>> "))
-        except ValueError:
-            print("\nPlease specify the length using interger\n")
+
+        choice = input("Include lowercase letters in password (y/n)\n>>> ")
+
+        if not choice.isalpha():
+            print("\nPlease enter alphabet")
             continue
-        if length < 0:
-            print("\nPlease Enter Positive Integer\n")
+        elif choice.lower() not in ['y','n']:
+            print("\nPlease enter letter 'y' for yes or 'n' for no")
             continue
         else:
             break
     
-    choice = []
-    for i in range(4):
-        while True:
-            if i == 0:
-                choice[i] = input("Include uppercase letters in password (y/n)\n>>> ")
-            elif i == 1:
-                choice[i] = input("Include lowercase letters in password (y/n)\n>>> ")
-            elif i == 2:
-                choice[i] = input("Include number in password (y/n)\n>>> ")
-            elif i == 3:
-                choice[i] = input("Include special letters in password (y/n)\n>>> ")
+    if choice == 'n':
+        return random_char
+        
+    random_char += string.ascii_lowercase
 
-            if not choice[i].isalpha():
-                print("\nPlease enter alphabet\n")
+    return random_char
+
+def upper_case(random_char):
+
+    while True:
+
+        choice = input("Include uppercase letters in password (y/n)\n>>> ")
+
+        if not choice.isalpha():
+            print("\nPlease enter alphabet")
+            continue
+        elif choice.lower() not in ['y','n']:
+            print("\nPlease enter letter 'y' for yes or 'n' for no")
+            continue
+        else:
+            break
+    
+    if choice == 'n':
+        return random_char
+        
+    random_char += string.ascii_uppercase
+
+    return random_char
+
+def digit(random_char):
+
+    while True:
+
+        choice = input("Include number in password (y/n)\n>>> ")
+
+        if not choice.isalpha():
+            print("\nPlease enter alphabet")
+            continue
+        elif choice.lower() not in ['y','n']:
+            print("\nPlease enter letter 'y' for yes or 'n' for no")
+            continue
+        else:
+            break
+    
+    if choice == 'n':
+        return random_char
+        
+    random_char += string.digits
+
+    return random_char
+
+def special_char(random_char):
+
+    while True:
+
+        choice = input("Include special letters in password (y/n)\n>>> ")
+
+        if not choice.isalpha():
+            print("\nPlease enter alphabet")
+            continue
+        elif choice.lower() not in ['y','n']:
+            print("\nPlease enter letter 'y' for yes or 'n' for no")
+            continue
+        else:
+            break
+    
+    if choice == 'n':
+        return random_char
+        
+    random_char += string.punctuation
+
+    return random_char
+
+def generate_password(length: int, random_char):
+
+    passwords = []
+
+    for i in range(4):
+        password = ""
+        for j in range(length):
+            password += random.choice(random_char)
+        passwords.append(password)
+
+    return passwords
+
+def main():
+
+    while True:
+
+        print("\nRandom password generator\n")
+
+        random_char = ""
+
+        while True:
+            try:
+                length = int(input("Specify the length of your passowrd\n>>> "))
+            except ValueError:
+                print("Please specify the length using interger\n")
                 continue
-            elif choice[i].lower() not in ['y','n']:
-                print("\nPlease enter letter 'y' for yes or 'n' for no\n")
+            if length < 0:
+                print("Please Enter Positive Integer\n")
                 continue
             else:
                 break
+        
+        random_char = lower_case(random_char)
+        random_char = upper_case(random_char)
+        random_char = digit(random_char)
+        random_char = special_char(random_char)
 
-    value = []
-    for (index, char) in enumerate(choice):
-        if char == "y":
-            value[index] = True
+        if not random_char:
+            print("\nPlease select something!!!\n")
+            choice = input("continue generatine password?\nPress 'n' to stop\n>>> ")
+            if choice == 'n':
+                break
+            else:
+                continue
+
+        passwords = generate_password(length, random_char)
+
+        print("\nYour genrated password are")
+        for password in passwords:
+            print(f">>> {password}")
+
+        choice = input("continue generatine password?\nPress 'n' to stop\n>>> ")
+
+        if choice == 'n':
+            break
         else:
-            value[index] = False
-    
-    passwords = generate_password(length, value[0], value[1], value[2], value[3])
+            continue
 
-    print("Your genrated password are")
-    for password in passwords:
-        print(f">>> {password}")
 
 if __name__ == "__main__":
     main()
