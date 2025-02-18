867273dc45b52203948b7e6002133e889b4b476c
UCAS-John <john.wangwang@ucas-edu.net>
2025-02-18 13:03:27 -0700

    change path

---
+++ notes/writing_text.py
@@ -0,0 +1,6 @@
+#How do you find a file in a folder? 
+
+#In a python project how do you open a file?
+with open():
+    pass
+#What is the difference between writing, appending, and creation modes?
