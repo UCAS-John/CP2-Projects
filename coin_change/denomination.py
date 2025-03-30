7e0b09bc84339a1f343b5823f67bc1afeb94b640
JohnRealNoob <nutt.john12@gmail.com>
2025-03-30 16:48:19 -0600

    Change to descendign order

--- coin_change/denomination.py
+++ coin_change/denomination.py
@@ -8,23 +8,26 @@ def solve_coin_change(country, target, coin_data):
         return target_cents, denoms_cents
 
     def coin_change(target_cents, denoms_cents):
-        if target_cents < 0 or not denoms_cents: # return None if target amount is 0 or coin denomination is empty
-            return None, float('inf')  
-        if target_cents == 0: # return 0 if target amount is 0
+        if target_cents < 0 or not denoms_cents:  # Return None if target amount is negative or no denominations
+            return None, float('inf')
+        if target_cents == 0:  # Return 0 if target amount is 0
             return {}, 0
+
+        # Sort denominations by value in descending order 
+        sorted_denoms = sorted(denoms_cents.items(), key=lambda x: x[1], reverse=True)
 
         coins_used = {}
         remaining_amount = target_cents
 
-        for coin_name, coin_value in denoms_cents.items():
+        for coin_name, coin_value in sorted_denoms:
             if coin_value <= remaining_amount:
                 num_coins = remaining_amount // coin_value
                 coins_used[coin_name] = num_coins
-                remaining_amount %= coin_value  
+                remaining_amount %= coin_value
 
-        # return None if remaining amount is not zero
+        # Return None if remaining amount is not zero
         if remaining_amount > 0:
-            return None, float('inf') 
+            return None, float('inf')
 
         return coins_used, sum(coins_used.values())
 
